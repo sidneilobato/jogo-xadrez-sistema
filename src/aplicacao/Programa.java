01a922efb58e2d2707069d4e67b002207ada4155
Sidnei Lobato <sidnei_lobato@hotmail.com>
2020-06-30 08:01:18 -0300

    Primeira Classe Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,13 @@
 package aplicacao;
 
+import mesatabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Bom dia!");
+	
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
